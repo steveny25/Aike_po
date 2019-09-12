c994c4f5396e737381569a3ae12aaa909146846b
steveny25 <steveny25@hotmail.com>
2019-09-12 11:16:13 +0800

    Initial commit

---
+++ scripts/test01.py
@@ -0,0 +1,16 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Author   : Daexiong
+# @Time     : 2019-09-12 11:12
+# @File     : test01.py
+# @Software : PyCharm
+
+class Test01:
+    def test001(self):
+        print("test001被执行")
+
+    def test002(self):
+        print("test002被执行")
+
+    def test003(self):
+        print("test003被执行")
