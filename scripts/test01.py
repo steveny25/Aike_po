892f666ec29fc99c6cd9e5c9107c804d6f9ad364
steveny25 <steveny25@hotmail.com>
2019-09-12 11:25:22 +0800

    update assert

--- scripts/test01.py
+++ scripts/test01.py
@@ -5,6 +5,7 @@
 # @File     : test01.py
 # @Software : PyCharm
 
+
 class Test01:
     def test001(self):
         print("test001被执行")
@@ -14,3 +15,4 @@ class Test01:
 
     def test003(self):
         print("test003被执行")
+        assert 5 != 2
